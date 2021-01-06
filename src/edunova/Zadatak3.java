ae459b9a6e000270527c4abfa66d35ac9862f5b6
Marija Bralić <marija.bralic96@gmail.com>
2021-01-06 21:59:26 +0100

    Rješenje trećeg zimskog zadatka.

---
+++ src/edunova/Zadatak3.java
@@ -0,0 +1,57 @@
+package edunova;
+
+import java.util.Scanner;
+
+public class Zadatak3 {
+
+    public static void main(String[] args) {
+
+        // Kreirati program koji za dva unesena broja ispisuje sve prim/prosti/prime brojeve između njih.
+
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println(Math.sqrt(101));
+
+        System.out.println("Unesi broj 1: ");
+        int x = ulaz.nextInt();
+
+        System.out.println("Unesi broj 2: ");
+        int y = ulaz.nextInt();
+
+        int poc = Math.min(x, y);
+        int kraj = Math.max(x, y);
+
+        for (int i = poc; i < kraj; i++) {
+            if (i <= 1) {
+                System.out.println(i + " Nije");
+            } else if (i == 2) {
+                System.out.println(i + " Je");
+            } else if (i % 2 == 0) {
+                System.out.println(i + " Nije");
+            } else {
+
+                boolean bo = false;
+
+                for (int n = 3; n <= Math.sqrt(i); n += 2) {
+                    if (i % n == 0) {
+                        bo = true;
+                        System.out.println(i + " Nije");
+                        break;
+                    }
+                }
+                if(!bo) {
+                    System.out.println(i + " Je");
+                }
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
